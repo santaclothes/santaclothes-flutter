b2d0760745c0d06d169a46d771235b56ba104c5c
YKW93 <rhkddn1657@gmail.com>
2021-05-07 14:00:12 +0900

    feat: splash 에서 토큰 유효성 검사 로직 추가

--- lib/presentation/splash/splash_controller.dart
+++ lib/presentation/splash/splash_controller.dart
@@ -1,10 +1,28 @@
 import 'package:get/get.dart';
+import 'package:santaclothes/data/model/token_response.dart';
+import 'package:santaclothes/data/repository/auth_repository.dart';
 import 'package:santaclothes/routes/app_routes.dart';
 
 class SplashController extends GetxController {
+  final AuthRepository _authRepository;
+
+  SplashController(this._authRepository);
+
   @override
   void onReady() {
     super.onReady();
-    Get.offNamed(Routes.LOGIN);
+    _checkUserToken();
+  }
+
+  _checkUserToken() async {
+    final TokenResponse? tokenResponse = await _authRepository.getToken();
+    await Future.delayed(Duration(seconds: 2));
+    if (tokenResponse == null ||
+        DateTime.parse(tokenResponse.expiredAt).millisecondsSinceEpoch <
+            DateTime.now().millisecondsSinceEpoch) {
+      Get.offNamed(Routes.LOGIN);
+    } else {
+      Get.offNamed(Routes.DASHBOARD);
+    }
   }
 }
