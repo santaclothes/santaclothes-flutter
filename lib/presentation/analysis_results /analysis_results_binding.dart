5a954c0e6aefeab6a3a8af9a5517de19807c762a
YKW93 <rhkddn1657@gmail.com>
2021-06-14 18:52:09 +0900

    feat: 분석 결과 화면 controller에 repository, 데이터 조회 로직 추가

--- lib/presentation/analysis_results /analysis_results_binding.dart
+++ lib/presentation/analysis_results /analysis_results_binding.dart
@@ -1,9 +1,10 @@
 import 'package:get/get.dart';
+import 'package:santaclothes/data/repository/analysis_repository.dart';
 import 'package:santaclothes/presentation/analysis_results%20/analysis_results_controller.dart';
 
 class AnalysisResultsBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put(AnalysisResultsController());
+    Get.put(AnalysisResultsController(AnalysisRepository()));
   }
 }
--- lib/presentation/analysis_results /analysis_results_controller.dart
+++ lib/presentation/analysis_results /analysis_results_controller.dart
@@ -1,3 +1,41 @@
 import 'package:get/get.dart';
+import 'package:santaclothes/data/model/analysis_request_result_response.dart';
+import 'package:santaclothes/data/repository/analysis_repository.dart';
+import 'package:santaclothes/utils/constants.dart';
 
-class AnalysisResultsController extends GetxController {}
+class AnalysisResultsController extends GetxController {
+  final AnalysisRepository _analysisRepository;
+
+  final isLoading = true.obs;
+
+  final analysisResults = Rxn<AnalysisRequestResultResponse>();
+
+  int requestId = -1;
+
+  AnalysisResultsController(this._analysisRepository);
+
+  @override
+  void onReady() {
+    super.onReady();
+    _fetchAnalysisRequestApi();
+  }
+
+  void _fetchAnalysisRequestApi() async {
+    try {
+      requestId = Get.arguments["requestId"];
+      if (requestId == -1) {
+        Get.back();
+        Get.snackbar("화면 전환 실패", DEFAULT_ERROR_MSG);
+        return;
+      }
+      analysisResults.value =
+          await _analysisRepository.getAnalysisRequest(requestId);
+      this.analysisResults.refresh();
+      isLoading.value = false;
+    } catch (e) {
+      isLoading.value = false;
+      Get.back();
+      Get.snackbar("화면 전환 실패", DEFAULT_ERROR_MSG);
+    }
+  }
+}
