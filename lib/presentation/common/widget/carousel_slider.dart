b2d481c95212263d06fd6ee53d795045cab5d6e9
YKW93 <rhkddn1657@gmail.com>
2021-06-14 16:18:05 +0900

    feat: carousel image slide 추가  - 서버에서 받은 이미지 url 값들을 보여줌

---
+++ lib/presentation/common/widget/carousel_slider.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:santaclothes/utils/constants.dart';
+import 'package:santaclothes/utils/size_config.dart';
+
+class CarouselSlider extends StatelessWidget {
+  final List<String> imageUrls;
+
+  CarouselSlider(this.imageUrls);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: getProportionateScreenWidth(250.0),
+          child: ListView.builder(
+            itemCount: imageUrls.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return _buildImageItemWidget(index);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildImageItemWidget(int position) {
+    return Container(
+      margin: position == 0
+          ? EdgeInsets.only(left: getProportionateScreenWidth(30.0))
+          : EdgeInsets.only(
+              left: getProportionateScreenWidth(14.0),
+              right: getProportionateScreenWidth(14.0),
+            ),
+      width: getProportionateScreenWidth(250.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: Image.network(
+          imageUrls[position],
+          loadingBuilder: (BuildContext context, Widget child,
+              ImageChunkEvent? loadingProgress) {
+            if (loadingProgress == null) {
+              return child;
+            }
+            return Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(primaryColor),
+                value: loadingProgress.expectedTotalBytes != null
+                    ? loadingProgress.cumulativeBytesLoaded /
+                        loadingProgress.expectedTotalBytes!
+                    : null,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
