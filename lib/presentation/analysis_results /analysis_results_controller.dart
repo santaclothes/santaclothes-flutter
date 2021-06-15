9c14f71a6d4200860814367a190593d362f0ee7c
YKW93 <rhkddn1657@gmail.com>
2021-06-15 13:54:13 +0900

    feat: 뷰에서 이미지와 라벨 이미지를 리스트 형식으로 보여주기 위한 따로 리스트로 분리

--- lib/presentation/analysis_results /analysis_results_controller.dart
+++ lib/presentation/analysis_results /analysis_results_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:santaclothes/data/model/analysis_request_result_response.dart';
+import 'package:santaclothes/data/network/dio_client.dart';
 import 'package:santaclothes/data/repository/analysis_repository.dart';
 import 'package:santaclothes/utils/constants.dart';
 
@@ -9,6 +10,8 @@ class AnalysisResultsController extends GetxController {
   final isLoading = true.obs;
 
   final analysisResults = Rxn<AnalysisRequestResultResponse>();
+
+  final analysisImageResults = <String>[].obs;
 
   int requestId = -1;
 
@@ -28,9 +31,15 @@ class AnalysisResultsController extends GetxController {
         Get.snackbar("화면 전환 실패", DEFAULT_ERROR_MSG);
         return;
       }
-      analysisResults.value =
-          await _analysisRepository.getAnalysisRequest(requestId);
-      this.analysisResults.refresh();
+      final result = await _analysisRepository.getAnalysisRequest(requestId);
+
+      // 의류 이미지 + 라벨 이미지
+      analysisImageResults.add(BASE_URL + result.clothImageUrl);
+      analysisImageResults
+          .addAll(result.careLabelImageUrls.map((e) => BASE_URL + e));
+
+      analysisResults.value = result;
+      analysisResults.refresh();
       isLoading.value = false;
     } catch (e) {
       isLoading.value = false;
