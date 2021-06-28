1e867d888ede3a88d6f815570d2a236d7e9d9b8a
YKW93 <rhkddn1657@gmail.com>
2021-06-28 21:59:51 +0900

    feat: 오류 레포트 화면 UI 작업 완료

---
+++ lib/presentation/error_report/error_report_body.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:santaclothes/presentation/common/widget/receipt_widget.dart';
+import 'package:santaclothes/presentation/common/widget/vertical_spacing.dart';
+import 'package:santaclothes/presentation/error_report/error_report_controller.dart';
+import 'package:santaclothes/utils/constants.dart';
+import 'package:santaclothes/utils/size_config.dart';
+
+class ErrorReportBody extends GetView<ErrorReportController> {
+  @override
+  Widget build(BuildContext context) {
+    return ReceiptWidget(
+      color: Colors.white,
+      allowsTopPaint: false,
+      child: Container(
+        margin: EdgeInsets.only(top: getProportionateScreenHeight(48.0)),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(30.0),
+              ),
+              child: RichText(
+                text: TextSpan(
+                  text: '잠깐! 촬영한 라벨과\n인신한 라벨이 ',
+                  style: TextStyle(
+                    fontSize: getProportionateScreenHeight(24.0),
+                    fontFamily: 'nanum_square',
+                    color: sancleDarkColor,
+                    fontWeight: FontWeight.w400,
+                    height: 1.2,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: '일치',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    TextSpan(
+                      text: '하나요?',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            VerticalSpacing(of: 20.0),
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(30.0),
+              ),
+              child: Text(
+                '일치하지 않은 라벨이 있다면 해당하는\n아이콘을 클릭하여 레포트를 보내주세요.',
+                style: TextStyle(
+                  fontSize: getProportionateScreenHeight(14.0),
+                  fontFamily: 'nanum_square',
+                  color: sancleDarkColor,
+                  fontWeight: FontWeight.w300,
+                  height: 1.2,
+                ),
+              ),
+            ),
+            VerticalSpacing(of: 40.0),
+            Container(
+              height: getProportionateScreenHeight(110.0),
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                physics: BouncingScrollPhysics(),
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return _careLabelItem(index);
+                },
+              ),
+            ),
+            _errorReportButton(),
+            VerticalSpacing(of: 28.0),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _careLabelItem(int position) {
+    return Container(
+      margin: EdgeInsets.only(
+        left: position == 0 ? getProportionateScreenWidth(30.0) : 0,
+        right: getProportionateScreenWidth(30.0),
+      ),
+      child: Column(
+        children: [
+          Image.network(
+            'url',
+            width: getProportionateScreenWidth(34.0),
+            height: getProportionateScreenWidth(34.0),
+          ),
+          VerticalSpacing(of: 13.0),
+          Text(
+            '라벨 이름',
+            style: TextStyle(
+              fontSize: getProportionateScreenHeight(12.0),
+              fontFamily: 'nanum_square',
+              color: sancleDarkColor,
+              fontWeight: FontWeight.w400,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _errorReportButton() {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: getProportionateScreenWidth(30.0),
+      ),
+      decoration: BoxDecoration(
+        color: whiteColor,
+        borderRadius: BorderRadius.all(
+          Radius.circular(30.0),
+        ),
+        border: Border.all(
+          width: 0.5,
+          color: Color.fromRGBO(15, 16, 18, 0.4),
+        ),
+      ),
+      width: double.infinity,
+      height: getProportionateScreenHeight(52.0),
+      child: Center(
+        child: Text(
+          '오류 레포트 보내기',
+          style: TextStyle(
+            color: sancleDarkColor,
+            fontSize: getProportionateScreenHeight(16.0),
+            fontFamily: 'nanum_square',
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/error_report/error_report_screen.dart
+++ lib/presentation/error_report/error_report_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:santaclothes/presentation/common/widget/horizontal_spacing.dart';
+import 'package:santaclothes/presentation/common/widget/vertical_spacing.dart';
+import 'package:santaclothes/presentation/error_report/error_report_body.dart';
 import 'package:santaclothes/utils/constants.dart';
 import 'package:santaclothes/utils/size_config.dart';
 
@@ -33,7 +36,66 @@ class ErrorReportScreen extends StatelessWidget {
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
         child: Column(
-          children: [],
+          children: [
+            _headerLabelImage(),
+            ErrorReportBody(),
+            VerticalSpacing(of: 42.0),
+            _nextButton(),
+            VerticalSpacing(of: 30.0),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _headerLabelImage() {
+    return Container(
+      width: Get.size.width,
+      height: Get.size.width * 1.2,
+      child: Image.network("url"),
+    );
+  }
+
+  Widget _nextButton() {
+    return GestureDetector(
+      onTap: () {
+        // TODO 라벨 결과 페이지 전환
+      },
+      child: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: getProportionateScreenWidth(30.0),
+        ),
+        decoration: BoxDecoration(
+          color: whiteColor,
+          borderRadius: BorderRadius.all(
+            Radius.circular(30.0),
+          ),
+          border: Border.all(
+            width: 0.5,
+            color: Color.fromRGBO(15, 16, 18, 0.4),
+          ),
+        ),
+        width: double.infinity,
+        height: getProportionateScreenHeight(52.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '다음',
+              style: TextStyle(
+                color: sancleDarkColor,
+                fontSize: getProportionateScreenHeight(16.0),
+                fontFamily: 'nanum_square',
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            HorizontalSpacing(of: 13.0),
+            SvgPicture.asset(
+              'assets/icons/ic_next_guide.svg',
+              height: getProportionateScreenHeight(23.0),
+              width: getProportionateScreenHeight(23.0),
+            ),
+          ],
         ),
       ),
     );
