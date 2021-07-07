5ef33106f2623b180b57ae2f3a8b758879f13926
YKW93 <rhkddn1657@gmail.com>
2021-07-07 18:26:08 +0900

    feat: 모델 디렉터리 구조 변경을 위해 해당 모델클래스 삭제  - 추후 재설계 작업

--- lib/presentation/dashboard/dashboard_controller.dart
+++ lib/presentation/dashboard/dashboard_controller.dart
@@ -1,9 +1,5 @@
 import 'package:get/get.dart';
-import 'package:santaclothes/data/model/fcm_data.dart';
-import 'package:santaclothes/data/prefs/fcm_manager.dart';
 import 'package:santaclothes/presentation/dashboard/model/dash_board_tab_type.dart';
-import 'package:santaclothes/routes/app_routes.dart';
-import 'dart:developer' as developer;
 
 class DashboardController extends GetxController {
   var currentTabType = DashBoardTabType.HOME.obs;
@@ -12,18 +8,16 @@ class DashboardController extends GetxController {
   void onReady() async {
     // TODO: implement onReady
     super.onReady();
-    FcmData data = await FcmManager.instance.getCategory();
-    developer.log('data : ${data.category}', name: 'fcm');
 
-    switch(data.category){
-      case "result":
-        // TODO 결과 화면으로 라우팅
-        Get.offNamed(Routes.RESULT, arguments: data.id);
-        break;
-      case "notice":
-      // TODO 공지 화면으로 라우팅
-      // Get.offNamed(Routes.NOTICE, arguments: data.id);
-        break;
-    }
+    // switch(data.category){
+    //   case "result":
+    //     // TODO 결과 화면으로 라우팅
+    //     Get.offNamed(Routes.RESULT, arguments: data.id);
+    //     break;
+    //   case "notice":
+    //   // TODO 공지 화면으로 라우팅
+    //   // Get.offNamed(Routes.NOTICE, arguments: data.id);
+    //     break;
+    // }
   }
 }
