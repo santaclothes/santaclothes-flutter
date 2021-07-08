a0d0eeca84e90deb916c51a8f643778cc54ed564
YKW93 <rhkddn1657@gmail.com>
2021-07-08 14:39:21 +0900

    style: body 내 header widget 분리 작업

--- lib/presentation/analysis_request/widget/analysis_request_body.dart
+++ lib/presentation/analysis_request/widget/analysis_request_body.dart
@@ -21,59 +21,13 @@ class AnalysisRequestBody extends GetView<AnalysisRequestController> {
       width: double.infinity,
       child: Column(
         children: [
-          Container(
-            width: double.infinity,
-            padding: EdgeInsets.symmetric(
-              horizontal: getProportionateScreenWidth(30.0),
-            ),
-            height: getProportionateScreenHeight(84.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(
-                    top: getProportionateScreenHeight(4.0),
-                    bottom: getProportionateScreenHeight(2.0),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '마지막 STEP',
-                        style: TextStyle(
-                          color: sancleDark2Color,
-                          fontSize: getProportionateScreenHeight(22.0),
-                          fontFamily: 'nanum_square',
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                      Text(
-                        '분석 요청서를 작성하고\n결과를 받아보세요-!',
-                        style: TextStyle(
-                          color: sancleDark2Color,
-                          fontSize: getProportionateScreenHeight(16.0),
-                          fontFamily: 'nanum_square',
-                          fontWeight: FontWeight.w400,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Image.asset(
-                  'assets/images/bg_sancle_illustration_1.png',
-                )
-              ],
-            ),
-          ),
-          VerticalSpacing(of: 26.0),
           ReceiptWidget(
             color: Colors.white,
             width: double.infinity,
             child: Container(
               child: Column(
                 children: [
-                  VerticalSpacing(of: 34.0),
+                  VerticalSpacing(of: 55.0),
                   Row(
                     children: [
                       HorizontalSpacing(of: 30.0),
