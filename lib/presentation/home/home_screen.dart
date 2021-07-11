b9f13429f51fc9d8afb6444a519e0c9350da2e0c
YKW93 <rhkddn1657@gmail.com>
2021-07-11 19:28:23 +0900

    feat: 알림 리스트 페이지 연결 작업

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:santaclothes/presentation/home/home_controller.dart';
 import 'package:santaclothes/presentation/home/widget/home_notice_widget.dart';
 import 'package:santaclothes/presentation/home/widget/home_user_widget.dart';
+import 'package:santaclothes/routes/app_routes.dart';
 import 'package:santaclothes/utils/constants.dart';
 import 'package:santaclothes/utils/size_config.dart';
 import 'package:touchable_opacity/touchable_opacity.dart';
@@ -18,34 +19,27 @@ class HomeScreen extends GetView<HomeController> {
           top: false,
           child: Stack(children: <Widget>[
             Container(
-              child: Obx(()=>Image.asset(
-                controller.background.value,
-                width: MediaQuery
-                    .of(context)
-                    .size
-                    .width,
-                fit: BoxFit.fitWidth,
-              )),
+              child: Obx(() => Image.asset(
+                    controller.background.value,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.fitWidth,
+                  )),
             ),
             Positioned(
                 right: getProportionateScreenWidth(24),
                 top: getProportionateScreenHeight(58),
                 child: TouchableOpacity(
                   activeOpacity: 0.6,
-                  onTap: () {
-                    // TODO route push list
-                    // Get.offNamed(Routes.MYPAGE);
-                  },
-                  child: Obx(()=>SvgPicture.asset(
-                    controller.notiIcon.value,
-                    width: getProportionateScreenHeight(52),
-                    height: getProportionateScreenHeight(52),
-                  )),
+                  onTap: () => Get.toNamed(Routes.NOTIFICATION),
+                  child: Obx(() => SvgPicture.asset(
+                        controller.notiIcon.value,
+                        width: getProportionateScreenHeight(52),
+                        height: getProportionateScreenHeight(52),
+                      )),
                 )),
             HomeUserWidget(),
             HomeNoticeWidget(),
           ]),
-        )
-    );
+        ));
   }
 }
